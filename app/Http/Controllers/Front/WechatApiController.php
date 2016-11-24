5cb37f132906ee8d49f1c16e167687799006dcba
Field Rome <Field Rome>
2016-11-24 22:55:18 +0800

    get access_token 1

--- app/Http/Controllers/Front/WechatApiController.php
+++ app/Http/Controllers/Front/WechatApiController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Front;
 
 
+use Cache;
 use App\Http\Controllers\Controller;
 
 class WechatApiController extends Controller
 {
+    // check wechat is valid
     public function valid()
     {
         $echoStr = $_GET['echostr'];
@@ -15,6 +17,36 @@ class WechatApiController extends Controller
         if($this->checkSignature()){
             echo $echoStr;
             exit;
+        }
+    }
+
+    // get access_token
+    public function getAccessToken()
+    {
+        $access_token = Cache::get('access_token');
+        if(!$access_token){
+            $access_token = resoloveToken();
+        }
+        return $access_token;
+    }
+
+    //resolve access_token url, get access_token
+    private function resoloveToken()
+    {
+        $app_id = config("constants.APP_ID");
+        $app_secret = config("constants.APP_SECRET");
+        if(!$app_id && !$app_secret) {
+            throw new Exception("APP_ID and APP_SECRET not found", 1);
+        }
+
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$app_id&secret=$app_secret";
+        $res = json_decode($file_get_contents($url), true);
+        if(isset($res['access_token']))
+        {
+            Cache::put('access_token', $res['access_token'], (int)($res['expires_in']-60)/60);
+            return $res['access_token'];
+        }else {
+            throw new Exception("didn't get wechat access_token:".$res['errmsg'], 1);
         }
     }
 
--- config/constants.php
+++ config/constants.php
@@ -1,4 +1,6 @@
 <?php
     return [
     'TOKEN' => 'rome',      //wechat token defined by myself
+    'APP_ID' => 'wx2f1c464aa0e68675',
+    'APP_SECRET' => 'a28111b2f8768ca0e10b9dbf9e0adb66',
     ];
